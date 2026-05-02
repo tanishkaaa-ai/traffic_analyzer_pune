39de14720336d40a2ee239ef975965d0ef0f785e
Shlesha5847 <147795373+Shlesha5847@users.noreply.github.com>
2026-05-02 16:45:55 +0530

    encoded

---
+++ backend/label_encoding.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+# Load dataset
+df = pd.read_csv("final_dataset.csv")
+
+# Mapping (same everywhere)
+place_mapping = {
+    "Swargate": 0,
+    "Hinjewadi": 1,
+    "Kothrud": 2,
+    "Hadapsar": 3,
+    "University": 4
+}
+
+# Apply mapping
+df["origin"] = df["origin"].map(place_mapping)
+df["destination"] = df["destination"].map(place_mapping)
+
+# Save updated dataset
+df.to_csv("final_dataset_encoded.csv", index=False)
+
+print("Encoding done. Saved as final_dataset_encoded.csv")

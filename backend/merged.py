c0348b3ea0d7dc81c36eee8b2570d60864f5a9ab
Tanishka Jain <jain.tanishka0109@gmail.com>
2026-05-03 01:46:40 +0530

    20 min final

--- backend/merged.py
+++ backend/merged.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 df1 = pd.read_csv("final_dataset_encoded.csv")
-df2 = pd.read_csv("final_dataset_encoded11.csv")
+df2 = pd.read_csv("final_dataset_encoded20.csv")
 
 merged = pd.concat([df1, df2], ignore_index=True)
-merged.to_csv("merged.csv", index=False)
+merged.to_csv("final_dataset_encoded.csv", index=False)
